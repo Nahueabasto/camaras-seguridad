0f17f904b004375749e5643c729ac83a033bcba3
Nahueabasto <85427134+Nahueabasto@users.noreply.github.com>
2023-10-12 16:19:56 -0300

    camaras

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-//import './App.css';
-import Inicio from './pages/Inicio';
+import './App.css';
+import Inicio from './Pages/Inicio';
 
 
 function App() {
   
   return (
     <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={Inicio} /> 
-    </Switch>
+    <div className="App"> 
+        <Switch>
+          <Route exact path="/" component={Inicio} />
+       
+        </Switch>
+      </div>
     </BrowserRouter>
   );
 }
